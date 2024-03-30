6d88f8b7b8306b69490c6de220cf005345dc1951
Emerson Jesus <Emersoncruz73@hotmail.com>
2024-03-30 13:41:36 -0300

    add header

---
+++ assets/js/global.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/**
+ * Add event on multiple elements
+ */
+
+const addEventOnElements = function (elements, eventType, callback)  {
+    for(const elem  of elements) elem.addEventListener(eventType, callback)
+}
+
+/**
+ * Toggle search box in mobile device || small screen
+ */
+
+const searchBox = document.querySelector("[search-box]")
+const searchTogglers = document.querySelectorAll("[search-toggler]")
+
+
+addEventOnElements(searchTogglers, "click", function () {
+    searchBox.classList.toggle("active")
+})
